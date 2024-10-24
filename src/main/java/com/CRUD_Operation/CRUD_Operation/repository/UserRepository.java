eacaf62bf272771986b964606368c4045dbf13ad
Ajinkya Satkar <87180291+ajsat007@users.noreply.github.com>
2024-10-24 18:24:25 +0530

    Delete src directory

--- src/main/java/com/CRUD_Operation/CRUD_Operation/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.CRUD_Operation.CRUD_Operation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.CRUD_Operation.CRUD_Operation.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
